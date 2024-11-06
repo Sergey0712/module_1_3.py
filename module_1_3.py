41f454b27a23e494317011ecaae41d7d9871bb8a
Sergey0712 <5337419ss07@gmail.com>
2024-11-06 17:21:30 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+from operator import truediv
+from tkinter.font import names
+
+name = 'name: Sergey'
+print(name)
+age = int(43)
+print(age)
+age = int(age+2)
+print(age)
+is_student = True
+print(is_student)
