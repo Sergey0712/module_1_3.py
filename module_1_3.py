7a34fa76042906ff19156f4582621bef5f9b25d6
Sergey0712 <5337419ss07@gmail.com>
2024-11-07 17:33:30 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -1,11 +1,11 @@
 from operator import truediv
 from tkinter.font import names
 
-name = 'name: Sergey'
-print(name)
+name = 'Sergey'
+print('name:', name)
 age = int(43)
-print(age)
+print('age:', age)
 age = int(age+2)
-print(age)
+print('new_age:', age)
 is_student = True
-print('is_student')
+print('is_student:', is_student)
